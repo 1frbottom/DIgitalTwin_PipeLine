efb174869be63eba63a04fd988ede05d8b3971ae
1frbottom <logballin1301@gmail.com>
2025-10-04 01:00:38 +0900

    first test command : [producer terminal] python producer.py /  [processor terminal] docker logs spark-submit -f

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DIgitalTwin_PipeLine
+
+- [producer terminal] python producer.py
+- [processor terminal] docker logs spark-submit -f 
+- 
---
+++ producer/producer.py
@@ -0,0 +1,27 @@
+# producer.py
+import time
+import json
+import random
+from kafka import KafkaProducer
+
+# 카프카 서버 주소
+bootstrap_servers = ['localhost:9092']
+# 카프카 토픽 이름 (데이터가 쌓일 채널)
+topic_name = 'traffic-data'
+
+producer = KafkaProducer(
+    bootstrap_servers=bootstrap_servers,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+print("Sending messages...")
+while True:
+    # 가상 데이터 생성 (차량 수)
+    vehicle_count = random.randint(1, 50)
+    message = {'vehicle_count': vehicle_count, 'timestamp': time.time()}
+
+    # 'traffic-data' 토픽으로 메시지 전송
+    producer.send(topic_name, value=message)
+    print(f"Sent: {message}")
+
+    time.sleep(1)
